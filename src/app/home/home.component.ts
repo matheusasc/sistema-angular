11b353d57fed502c856d709150a409b7d3f395dc
Matheus Assunção <matheus.assuncao994@gmail.com>
2023-08-24 23:55:11 -0400

    Botão para baixar pdf da visualização do ccaf feito

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ElementRef, ViewChild} from "@angular/core";
+import * as jspdf from "jspdf";
+import jsPDF from "jspdf";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  @ViewChild('pdf', {static: false}) el!: ElementRef;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
 
 }
--- src/app/tela-ccaf/tela-ccaf.component.ts
+++ src/app/tela-ccaf/tela-ccaf.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ElementRef, ViewChild} from "@angular/core";
+import jsPDF from "jspdf";
 
 @Component({
   selector: 'app-tela-ccaf',
   templateUrl: './tela-ccaf.component.html',
   styleUrls: ['./tela-ccaf.component.css']
 })
-export class TelaCcafComponent {
+export class TelaCcafComponent implements OnInit{
+
+  @ViewChild('ccaf', {static: false}) el!: ElementRef;
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  printPDF(){
+    let pdf = new jsPDF('p', 'pt', 'a4');
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("teste.pdf")
+      }
+    })
+  }
 
 }
